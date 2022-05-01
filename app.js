1e34a4006930c8198ec144274eae5975de4c48f1
LarsOwens <104724741+LarsOwens@users.noreply.github.com>
2022-05-01 10:43:35 -0700

    Simple version done

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const colors = ["green", "red", "rgba(133,122,200)",
 
  btn.addEventListener('click', function () {
      // get random between 0 - 3 
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber]; 
 
  });
+
+ function getRandomNumber() {
+     return Math.floor(Math.random() * colors.length);
+ }
